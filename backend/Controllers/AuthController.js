941c28334b9fb4ca9121c1340d5b33fec3882674
Sanket Girhepunje <sanketgirhepunje@gmail.com>
2025-07-15 13:11:25 +0530

    bcryptjs changes

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const UserModel = require("../Models/User");
 
@@ -12,7 +12,7 @@ const signup = async (req, res) => {
                 .json({ message: 'User is already exist, you can login', success: false });
         }
         const userModel = new UserModel({ name, email, password });
-        userModel.password = await bcrypt.hash(password, 10);
+        userModel.password = await bcryptjs.hash(password, 10);
         await userModel.save();
         res.status(201)
             .json({
